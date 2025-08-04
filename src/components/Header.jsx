be40012895126ab00f0d6eac6a53ea393472c3ac
createdbyamir <amirabdulla853@gmail.com>
2025-08-04 18:58:18 +0100

    Fix logo path for live site compatibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 function Header() {
     return (
         <header className="mb-10">
-            <img src="./public/logo.png" alt="Logo" width="200" className="m-auto"/>
+            <img src="/logo.png" alt="Logo" width="200" className="m-auto"/>
         </header>
     );
 }
